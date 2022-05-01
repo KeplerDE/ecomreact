8b6012c53c4a7468f643b21391f0755c9e63c751
Dennny <keplerde@gmail.com>
2022-05-01 12:38:57 +0200

    Implementing React Router

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,21 +1,28 @@
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 function Header() {
-  return (  
+  return (
     <header>
        <Navbar bg="dark" variant="dark" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">Denny Shop</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>Home</Nav.Link>
-        <Nav.Link href="/login"><i className="fas fa-user"></i>Link</Nav.Link>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+           <Container>
+               <LinkContainer to='/'>
+                <Navbar.Brand >Denny Shop</Navbar.Brand>
+               </LinkContainer>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <LinkContainer to='/cart'>
+                            <Nav.Link><i className="fas fa-shopping-cart"></i>Cart</Nav.Link>
+                        </LinkContainer>
+                        <LinkContainer to='/login'>
+                            <Nav.Link><i className="fas fa-user"></i>Link</Nav.Link>
+                         </LinkContainer>
+                    </Nav>
+                </Navbar.Collapse>
+           </Container>
+       </Navbar>
     </header>
   )
 }
